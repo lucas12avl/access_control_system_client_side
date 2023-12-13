cfc49d5cc99a4112df150d79de5c8fad584ad98a
lucas12avl <73055673+lucas12avl@users.noreply.github.com>
2023-12-13 13:55:26 +0100

    Initial commit

---
+++ lib/screen_space.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:tutorial_acs_2/tree.dart';
+import 'package:tutorial_acs_2/requests.dart';
+
+class ScreenSpace extends StatefulWidget {
+  final String id;
+  const ScreenSpace({super.key, required this.id});
+
+  @override
+  State<ScreenSpace> createState() => _StateScreenSpace();
+}
+
+class _StateScreenSpace extends State<ScreenSpace> {
+  late Future<Tree> futureTree;
+
+  @override
+  void initState() {
+    super.initState();
+    futureTree = getTree(widget.id);
+  }
+
+  Widget _buildRow(Door door, int index) {
+    return ListTile(
+      title: Text('D ${door.id}'),
+      trailing: Text('${door.state}, closed=${door.closed}'),
+    );
+  }
+
+  // future with listview
+// https://medium.com/nonstopio/flutter-future-builder-with-list-view-builder-d7212314e8c9
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<Tree>(
+      future: futureTree,
+      builder: (context, snapshot) {
+        // anonymous function
+        if (snapshot.hasData) {
+          return Scaffold(
+            appBar: AppBar(
+              backgroundColor: Theme.of(context).colorScheme.primary,
+              foregroundColor: Theme.of(context).colorScheme.onPrimary,
+              title: Text(snapshot.data!.root.id),
+              actions: <Widget>[
+                IconButton(icon: const Icon(Icons.home), onPressed: () {}
+                  // TODO go home page = root
+                ),
+                //TODO other actions
+              ],
+            ),
+            body: ListView.separated(
+              // it's like ListView.builder() but better because it includes a separator between items
+              padding: const EdgeInsets.all(16.0),
+              itemCount: snapshot.data!.root.children.length,
+              itemBuilder: (BuildContext context, int i) =>
+                  _buildRow(snapshot.data!.root.children[i], i),
+              separatorBuilder: (BuildContext context, int index) =>
+              const Divider(),
+            ),
+          );
+        } else if (snapshot.hasError) {
+          return Text("${snapshot.error}");
+        }
+        // By default, show a progress indicator
+        return Container(
+            height: MediaQuery.of(context).size.height,
+            color: Colors.white,
+            child: Center(
+              child: CircularProgressIndicator(),
+            ));
+      },
+    );
+  }
+
+
+
+}
